f27417d833b81da0c24a10a3d96dfab26d55b3ec
Bianca Torres <biancatorres@edu.unifor.br>
2024-10-02 10:06:46 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    // Exibe uma mensagem dentro do elemento <p>
+    document.getElementById('message').innerText = 'Olá! Você clicou no botão!';
+    
+    // Exibe uma mensagem de alerta
+    alert('Botão clicado!');
+}
